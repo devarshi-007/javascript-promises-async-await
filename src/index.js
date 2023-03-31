ceabb56c48641d6aa84e650733cf7ecfd4dc11e2
devarshi007 <devarshi.trivedi@improwised.com>
2023-03-31 11:24:17 +0530

    Added task 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import { fetchWithTimeout } from "./services.js";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+let movies = require ("./data/movies.json")
+
 export function fetchMovies(){
-    
-} 
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction)
+}
+
+let moviePromise = fetchMovies()
+moviePromise.then(movies => console.log(movies))
