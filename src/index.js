675ff671f3a2ed23fcc913bc3d738455f26bcb18
devarshi007 <devarshi.trivedi@improwised.com>
2023-04-03 10:39:23 +0530

    feat:Wroking branch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import { fetchWithTimeout } from "./services";
+import { fetchMovies } from "./services";
+import { fetchBooks } from "./services";
 
 let movies = require ("./data/movies.json")
 
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction)
+function getBooksAndMovies(){
+    return Promise.all([fetchBooks(),fetchMovies()]).then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
 }
 
-let moviePromise = fetchMovies()
-moviePromise.then(
+const getBooksAndMoviesPromise = getBooksAndMovies()
+getBooksAndMoviesPromise.then(
     function resolveCallback(results){
-        console.log(results)
+        console.log('getBooksAndMoviesPromise', results);
     }
 )
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(),fetchMovies()]).then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies()
+getBooksOrMoviesPromise.then(
+    function resolveCallback(results){
+        console.log('getBooksOrMoviesPromise', results);
+    }
+)
+//Task-2
+// export function fetchMovies(){
+//     const resolveFunction = () => movies;
+//     return fetchWithTimeout(1000).then(resolveFunction)
+// }
+
+// let moviePromise = fetchMovies()
+// moviePromise.then(
+//     function resolveCallback(results){
+//         console.log(results)
+//     }
+// )
